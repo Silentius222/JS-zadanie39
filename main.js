ac95e0852bf6545ae10421407b6bbdae306b312a
Silentius222 <79799875+Silentius222@users.noreply.github.com>
2022-06-14 14:19:31 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-let tablica = [1, 2, 3, 4, 5];
+let array = [1, 2, 3, 4, 5];
 
 function funkcja(tablica){
   let pierwszy = tablica[0];
+  return pierwszy;
 }
 
-console.log(pierwszy);
+console.log(array);
